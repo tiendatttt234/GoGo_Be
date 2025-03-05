c777ea5e2f53d2a57a0e49e6ae8917e7d24363e8
tiendatttt234 <nguyentiendat23402@gmail.com>
2025-03-05 10:42:08 +0700

    be

--- index.js
+++ index.js
@@ -1,49 +1,53 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import mongoose from 'mongoose'
-import cors from 'cors'
-import cookieParser from 'cookie-parser'
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-import tourRoute from './routes/tours.js'
-import userRoute from './routes/users.js'
-import authRoute from './routes/auth.js'
-import reviewRoute from './routes/reviews.js'
+import tourRoute from "./routes/tours.js";
+import userRoute from "./routes/users.js";
+import authRoute from "./routes/auth.js";
+import reviewRoute from "./routes/reviews.js";
 
-
-dotenv.config()
-const app = express()
-const port = process.env.PORT || 4000 // Changed to 4000 to match frontend BASE_URL
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 4000; // Changed to 4000 to match frontend BASE_URL
 
 // database connection
-mongoose.set("strictQuery", false)
+mongoose.set("strictQuery", false);
 const connect = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        }) 
-        console.log("MongoDB database connected")
-    } catch (err) {
-        console.log("MongoDB database failed to connect")
-    }
-}
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB database connected");
+  } catch (err) {
+    console.log("MongoDB database failed to connect");
+  }
+};
 
-// middleware 
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors({
-    origin: 'http://localhost:3000', // or your client-side address
+// middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://go-go-fe-ljpj.vercel.app",
+      "https://go-go-fe.vercel.app/",
+    ], // or your client-side address
     credentials: true,
-}))
+  })
+);
 
 // routes
-app.use("/api/v1/auth", authRoute)
-app.use('/api/v1/tours', tourRoute)
-app.use("/api/v1/users", userRoute)
-app.use("/api/v1/reviews", reviewRoute)
-
+app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/tours", tourRoute);
+app.use("/api/v1/users", userRoute);
+app.use("/api/v1/reviews", reviewRoute);
 
 app.listen(port, () => {
-    connect()
-    console.log('server listening on port', port)
-})
+  connect();
+  console.log("server listening on port", port);
+});
