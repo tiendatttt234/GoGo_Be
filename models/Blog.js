aab0522f174f4b290300aa130178ea76d49f3b65
Dang Tung Duong <dangtungduong0207@gmail.com>
2025-03-11 15:15:40 +0700

    u

--- models/Blog.js
+++ models/Blog.js
@@ -40,10 +40,10 @@ const blogSchema = new mongoose.Schema(
       type: String,
       validate: {
         validator: function(v) {
-          // Validate YouTube, Vimeo or direct video URL
-          return !v || /^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.be|vimeo\.com)\/.+|^https?:\/\/.+\.(mp4|webm)$/.test(v);
+          // Validate YouTube, Vimeo, TikTok or direct video URL
+          return !v || /^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.be|vimeo\.com|tiktok\.com)\/.+|^https?:\/\/.+\.(mp4|webm)$/.test(v);
         },
-        message: 'Video URL must be a valid YouTube, Vimeo or direct video link'
+        message: 'Video URL must be a valid YouTube, Vimeo, TikTok or direct video link'
       }
     },
     author: {
